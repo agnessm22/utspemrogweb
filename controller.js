1274aa2470afc2585693cd3e1f9f4a3b38e94a7a
Agnes Sri Muliani <justagnes225@outlook.com>
2020-04-30 16:23:38 +0700

    berhasil menampilkan data tabel sparepart berdasarkan id

--- controller.js
+++ controller.js
@@ -6,3 +6,16 @@ var connection = require('./koneksi');
 exports.index = function(req, res){
     response.ok("UTS pemrograman website ku berjalan yey!",res)
 };
+
+//menampilkan semua data tabel sparepart berdasarkan id
+exports.tampilsemuaberdasarkanid = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM t_sparepart WHERE id_sparepart = ?', [id],
+    function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok(rows,res)
+        }
+    });
+};
